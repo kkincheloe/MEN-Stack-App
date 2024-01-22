dc57214c0d33eeea4a0c4413fce803cd55b35f4e
kkincheloe <151974270+kkincheloe@users.noreply.github.com>
2024-01-22 10:21:22 -0500

    finish

--- server.js
+++ server.js
@@ -22,16 +22,6 @@ const reviewsCtrl = require('./controllers/reviews')
 const app = express();
 
 
-/* Configure the app to refresh the browser when nodemon restarts
---------------------------------------------------------------- */
-// const liveReloadServer = livereload.createServer();
-// liveReloadServer.server.once("connection", () => {
-//     // wait for nodemon to fully restart before refreshing the page
-//     setTimeout(() => {
-//         liveReloadServer.refresh("/");
-//     }, 100);
-// });
-
 
 /* Configure the app (app.set)
 --------------------------------------------------------------- */
